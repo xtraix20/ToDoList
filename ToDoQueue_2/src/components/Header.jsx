b71f174bffde17c9de9b158e7c64d1aa75befacb
Xtraixx <juan.van.usa@gmail.com>
2025-07-11 20:29:32 -0500

    Fix Header navigation to use React Router Link instead of anchor tags

--- ToDoQueue_2/src/components/Header.jsx
+++ ToDoQueue_2/src/components/Header.jsx
@@ -1,38 +1,27 @@
-//import React from 'react'
+﻿//import React from 'react'
+import { Link } from 'react-router-dom'
 import rick from '../assets/rm.svg'
 import todo from '../assets/todo.svg'
 import '../styles/Header.sass'
-
 const Header =()=> {
   return (
       <>
-              
       <div className='uno'>
         <div className='container'>
-        
-           <div className='Logo' id= "to-do-logo" > 
-            <a href="/todo " target = "blank"> 
-             <img src= {todo} className='LogoToDo' alt= "to do list" ></img></a> 
-            
+           <div className='Logo' id= "to-do-logo" >
+            <Link to="/todo">
+             <img src= {todo} className='LogoToDo' alt= "to do list" ></img></Link>
             <p className='Title'> ToDoList </p>
             </div>
-            
             <div className='Logo'><h1 className='Title' id="DoTo">Do To Queue </h1></div>
-          <div className='Logo' id= "rick"> 
-            <a href="/RMCharacter" target ="blank">
-             <img src = {rick}className='rick' alt = "rick" ></img> </a>
-             
+          <div className='Logo' id= "rick">
+            <Link to="/RMCharacter">
+             <img src = {rick}className='rick' alt = "rick" ></img> </Link>
            <p className='Title5'> Rick and morty API </p>
-          
-           
             </div>
-          
-            
-
       </div>
       </div>
       </>
   )
 }
-
 export default Header
